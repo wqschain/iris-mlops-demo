dc3abedc05d6b012e93a2d4e4fe03696fa81837f
waqas <wwaqasrana@gmail.com>
2026-07-16 13:57:23 -0400

    added commenting to help explain my rationale

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from pydantic import BaseModel
 import joblib
 import logging 
 
-logging.basicConfig(
+
+# logging for the terminal and for a file to keep records
+logging.basicConfig(  
     level=logging.INFO,
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler("app.log")
+        logging.FileHandler("app.log")  
     ]
 )
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-model = joblib.load("model.pkl")
+model = joblib.load("model.pkl") # im loading the model ahead of time at the start to avoid constant repeat loading 
 
 class FlowerInput(BaseModel):
     sepal_length : float 
@@ -23,6 +25,7 @@ class FlowerInput(BaseModel):
 
 @app.post("/predict")
 def predict_species(flower: FlowerInput):
+    # a 2D array is needed for the neccesary input for scikit-learn
     flower_data = [[flower.sepal_length, flower.sepal_width, flower.petal_length, flower.petal_width]] 
     logger.info(f"Received prediction request: {flower_data}")
     try :
@@ -30,12 +33,14 @@ def predict_species(flower: FlowerInput):
         logger.info(f"Prediction result: {int(prediction[0])}")
         return int(prediction[0])
     except Exception as e: 
+                # catching unexpected errors and handling it cleanly
         logger.error(f"Prediction failed: {e}")
         raise HTTPException(status_code=500, detail="Prediction failed")
 
 
 @app.get("/health")
 def health_check():
-     return {"status": "ok"}
+    # seperate endpoint just to monitor uptime , very basic json return to confirm health
+    return {"status": "ok"}
     
     
--- train.py
+++ train.py
@@ -1,26 +1,24 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris #Imports
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
 
+
 iris = load_iris()
 
-print(iris.keys())
-print(iris.data[:5])
-print(iris.target[:5])
-print(iris.feature_names)
-print(iris.target_names)
-
-X_train , X_test, y_train, y_test = train_test_split( iris.data, iris.target)
+# im fixing the random state so every run of this code will result in the same split 
+X_train , X_test, y_train, y_test = train_test_split( iris.data, iris.target, random_state=42)
 print(len(X_train))
 print(len(X_test))
 
-model = RandomForestClassifier()
+# im also fixing the random state here too so the training of the model is the exact same each run 
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
-print(accuracy)
+print(f"Test Accuracy : {accuracy}")
 
+# This helps me "save" the model into a file so main.py can run without retraining each time 
 joblib.dump(model, "model.pkl")
